046daf10824f6ae321f788509f477b4400c2d4cb
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-26 13:38:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,5 +64,25 @@ end
     sql = <<-SQL
     student.new_from_db
     SQL
-    DB[:conn].execute(sql, name, grade)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.count_all_students_in_grade_9
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      end
   end
